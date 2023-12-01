a1ce6a086059012bc9bf21e9ce0e74da43d4a712
Yiosy <150297399+Yiosy@users.noreply.github.com>
2023-12-01 21:28:30 +0100

    added function that scales grid size correctly

--- js.js
+++ js.js
@@ -1,13 +1,30 @@
 const container = document.getElementById("grid");
 const box = document.getElementsByClassName("box");
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.className = "box";
-  container.appendChild(div);
+function gridSize(size) {
+  for (let i = 0; i < size ** 2; i++) {
+    const div = document.createElement("div");
+    div.style.flexBasis = 100 / size + "%";
+    div.className = "box";
+    container.appendChild(div);
+  }
 }
+
 container.addEventListener("click", (e) => {
   if (e.target.matches(".box")) {
     e.target.style.backgroundColor = "red";
   }
 });
+//
+
+// function gridSize(a) {
+//   gridSize = a ** 2;
+//   return gridSize;
+// }
+// function flexBasis(a) {
+//   flexBasis = 100 / a;
+//   return flexBasis;
+// }
+let size = prompt("asd");
+
+gridSize(size);
