4cc67c688100948d58d1d79277b1850cbbaeffd1
Yiosy <150297399+Yiosy@users.noreply.github.com>
2023-12-04 09:43:47 +0100

    Buttons now work correctly

--- js.js
+++ js.js
@@ -2,7 +2,8 @@ const container = document.getElementById("grid");
 const box = document.getElementsByClassName("box");
 const rainbow = document.getElementById("rainbow");
 const black = document.getElementById("black");
-
+const eraser = document.getElementById("eraser");
+let color = "black";
 function gridSize(size) {
   for (let i = 0; i < size ** 2; i++) {
     const div = document.createElement("div");
@@ -15,15 +16,18 @@ gridSize(8);
 
 container.addEventListener("mouseover", (e) => {
   if (e.target.matches(".box")) {
-    e.target.style.backgroundColor = randomColors();
+    if (color === "rainbow") {
+      e.target.style.backgroundColor = randomColors();
+      e.target.style.opacity = Number(e.target.style.opacity) + 0.3;
+    } else if (color === "black") {
+      e.target.style.backgroundColor = "black";
+      e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
+    } else if (color === "eraser") {
+      e.target.style.backgroundColor = null;
+      e.target.style.opacity = 0;
+    }
   }
 });
-
-// container.addEventListener("mouseover", (e) => {
-//   if (e.target.matches(".box")) {
-//     e.target.style.backgroundColor = "black";
-//   }
-// });
 
 const button = document.getElementById("Grid-size");
 
@@ -35,11 +39,11 @@ function removeAllChildNodes(parent) {
 
 button.addEventListener("click", () => {
   let a = prompt("type grid's size");
-  if (a <= 100) {
+  if (a <= 100 && a > 0) {
     removeAllChildNodes(container);
     gridSize(a);
   } else {
-    alert("number can't exceed 100");
+    alert("number can't exceed 100 or be below 0");
   }
 });
 function randomColors() {
@@ -47,3 +51,15 @@ function randomColors() {
     "#" + (randomColor = Math.floor(Math.random() * 16777215).toString(16))
   );
 }
+
+rainbow.addEventListener("click", () => {
+  color = "rainbow";
+});
+
+black.addEventListener("click", () => {
+  color = "black";
+});
+
+eraser.addEventListener("click", () => {
+  color = "eraser";
+});
